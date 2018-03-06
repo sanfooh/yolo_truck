25a6f9408b9009d5b43075e5adf69eef5c7b0d36
sanfooh <sanfooh@sina.com>
2018-03-06 21:56:38 +0800

    Update darknet.py

--- darknet.py
+++ darknet.py
@@ -36,7 +36,7 @@ class METADATA(Structure):
     
 
 #lib = CDLL("/home/pjreddie/documents/darknet/libdarknet.so", RTLD_GLOBAL)
-lib = CDLL(“../libdarknet.so", RTLD_GLOBAL)
+lib = CDLL("/darknet/libdarknet.so", RTLD_GLOBAL)
 lib.network_width.argtypes = [c_void_p]
 lib.network_width.restype = c_int
 lib.network_height.argtypes = [c_void_p]
@@ -133,9 +133,9 @@ if __name__ == "__main__":
     #meta = load_meta("cfg/imagenet1k.data")
     #r = classify(net, meta, im)
     #print r[:10]
-    net = load_net("cfg/YOLO-obj.cfg", "tiny-yolo.weights", 0)
-    meta = load_meta("cfg/obj.data")
-    r = detect(net, meta, “../test/dog.jpg")
-    print r
+    net = load_net(b"/darknet/work/cfg/YOLO-obj.cfg", b"/data/YOLO-obj_20000.weights", 0)
+    meta = load_meta(b"/darknet/work/cfg/obj.data")
+    r = detect(net, meta, "/darknet/test/dog.jpg")
+    print(r)
     
 
